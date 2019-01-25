6ab308e6a2ff2879ebb6ba52899324e1e26e41b4
Воробьева Елена Юрьевна <good-mark@mail.ru>
2019-01-26 00:06:31 +0300

    Start buttons added.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import urllib3
