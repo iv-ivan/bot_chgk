6ddf91514f63193f12cc0219fa95554237abf0e2
Ivan <ivan290895@yandex.ru>
2019-01-25 12:25:49 +0300

    Initial revision

---
+++ bot.py
@@ -0,0 +1,156 @@
+# coding: utf-8
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import urllib2
+import pandas as pd
+from BeautifulSoup import BeautifulSoup
+import time
+
+URL = "http://old1.club60sec.ru/calendar/6662/"
+FILE = "chgk_res.xls"
+args = {"LAST_UPDATE_TIME" : 0}
+update_interval = 5 * 60
+N_COMMANDS = 15
+LOGS_FILE="logs.txt"
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def track_metrica(handler):
+    def z(func):
+        def f(*args, **kwargs):
+            with open(LOGS_FILE, "a") as ggg:
+                ggg.write(str(time.time()) + " " + handler + "\n")
+            return func(*args, **kwargs)
+        return f
+    return z
+
+def get_doc():
+    #cache results
+    if time.time() - args["LAST_UPDATE_TIME"] < update_interval:
+        print "Cached"
+        return
+
+    response = urllib2.urlopen(URL)
+    soup = BeautifulSoup(response.read())
+
+    dropbox_href = None
+    for link in soup.findAll('a'):
+        link_text = link.get('href')
+        if "dropbox" in link_text:
+            dropbox_href = link_text
+            break
+
+    dropbox_href = "=".join(dropbox_href.split("=")[:-1] + ["1"])
+
+    response = urllib2.urlopen(dropbox_href)
+    with open(FILE, "w") as f:
+        args["LAST_UPDATE_TIME"] = time.time()
+        f.write(response.read())
+
+@track_metrica("start")
+def start(bot, update):
+    update.message.reply_text('Привет! Я умею показывать результаты ЧГК. Используй /help')
+
+@track_metrica("help")
+def help(bot, update):
+    commands = {"/results": "результаты по всем играм",
+                "/final_results": "результаты за вычетом 2 худших игр"}
+    update.message.reply_text("\n".join([k + " " + v for k, v in commands.iteritems()]))
+
+
+def getCommands(sheetX):
+    commands = map(unicode, sheetX[u'Команда'])
+    maxN = 0
+    while commands[maxN] != "nan":
+        maxN += 1
+    for i in xrange(maxN):
+        if commands[i] == u"Высокий пинг":
+            commands[i] = u"<b>Высокий пинг</b>"
+            break
+    return commands, maxN
+
+
+def parse_matrix(sheetX):
+    commands, maxN = getCommands(sheetX)
+    points = map(unicode, sheetX[u'сум'])[:maxN]
+    position = map(unicode, range(1, maxN + 1))
+    matrix_res = u"Матрица:\n" + u'\n'.join(map(lambda x: x[0] + ") " + x[1] + " - " + x[2], zip(position, commands[:maxN], points))[:N_COMMANDS])
+    return matrix_res
+
+@track_metrica("results")
+def results(bot, update):
+    get_doc()
+    xls = pd.ExcelFile(FILE)
+    sheetX = xls.parse(0)
+    commands, maxN = getCommands(sheetX)
+    points = map(unicode, sheetX[u'сум'])[:maxN]
+    position = map(unicode, range(1, maxN + 1))
+    chgk_res = u"ЧГК:\n" + u'\n'.join(map(lambda x: x[0] + ") " + x[1] + " - " + x[2], zip(position, commands, points))[:N_COMMANDS])
+
+    matrix_res = parse_matrix(xls.parse(1))
+
+    update.message.reply_html(chgk_res + u"\n\n" + matrix_res)
+
+
+def cast_x(x):
+	if x == u"nan":
+		return "0"
+	return x
+
+@track_metrica("final_results")
+def final_results(bot, update):
+    get_doc()
+    xls = pd.ExcelFile(FILE)
+    sheetX = xls.parse(0)
+    commands, maxN = getCommands(sheetX)
+
+    points = [[] for i in xrange(maxN)]
+    columns = sheetX.columns.values
+    for column in columns[2:]:
+   		points_col = map(unicode, sheetX[column])
+   		points_col = map(float, map(cast_x, points_col))
+   		for j in xrange(maxN):
+   			points[j].append(points_col[j])
+    for i in xrange(maxN):
+   		points[i].sort()
+   		points[i] = points[i][2:]
+   		points[i] = sum(points[i])
+    command_points = zip(commands[:maxN], points)
+    command_points.sort(key=lambda x: -x[1])
+    command_points = map(lambda x: x[0] + " - " + unicode(x[1]), command_points)
+
+    position = map(unicode, range(1, maxN + 1))
+    chgk_res = u"ЧГК:\n" + u'\n'.join(map(lambda x: x[0] + ") " + x[1], zip(position, command_points))[:N_COMMANDS])
+
+    matrix_res = parse_matrix(xls.parse(1))
+
+    update.message.reply_html(chgk_res + u"\n\n" + matrix_res)
+
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    updater = Updater("")
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("results", results))
+    dp.add_handler(CommandHandler("final_results", final_results))
+    dp.add_handler(MessageHandler(Filters.text, start))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
